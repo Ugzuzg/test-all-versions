163771138c2ee7f615ef03e787516edfc0598426
Jarasłaŭ Viktorčyk <ugzuzg@gmail.com>
2025-09-04 19:03:50 +0200

    Install all verions in linked mode

--- test.js
+++ test.js
@@ -25,7 +25,7 @@ helpOptions.forEach(function (option) {
     })
     processStdout(cp, function (code, lines) {
       t.equal(code, 0, 'should exit with exit code 0')
-      t.equal(lines.length, 11, 'should output 11 lines')
+      t.equal(lines.length, 12, 'should output 12 lines')
       t.ok(lines[0].startsWith('Usage: tav'))
       t.ok(lines[10].startsWith('  --ci')) // TODO: Use .at(-1) once Node.js 14 support is dropped
       t.end()
@@ -33,197 +33,204 @@ helpOptions.forEach(function (option) {
   })
 })
 
-t.test('tests succeed', function (t) {
-  const cp = exec('./index.js roundround "<=0.2.0" -- node -e "process.exit\\(0\\)"')
-  cp.on('close', function (code) {
-    t.equal(code, 0)
-    t.end()
-  })
-  cp.stdout.pipe(process.stdout)
-  cp.stderr.pipe(process.stderr)
-})
+const strategies = ['', '--experimental-isolated-installs']
+strategies.forEach(function (strategy) {
+  t.test(`strategy: ${strategy || 'default'}`, function (t) {
+    t.plan(11)
 
-t.test('tests fail', function (t) {
-  const cp = exec('./index.js roundround "<=0.2.0" -- node -e "process.exit\\(1\\)"')
-  cp.on('close', function (code) {
-    t.equal(code, 1)
-    t.end()
-  })
-  cp.stdout.pipe(process.stdout)
-  cp.stderr.pipe(process.stderr)
-})
-
-t.test('invalid module', function (t) {
-  const cp = exec('./index.js test-all-versions-' + Date.now() + ' ^1.0.0 npm test')
-  cp.on('close', function (code) {
-    t.equal(code, 1)
-    t.end()
-  })
-  cp.stdout.pipe(process.stdout)
-  cp.stderr.pipe(process.stderr)
-})
-
-t.test('yaml', function (t) {
-  const cp = start()
-
-  processStdout(cp, function (code, lines) {
-    t.equal(code, 0)
-    // TODO: Remove tap16 check when Node.js 14 support is dropped
-    t[TAP16 ? 'strictSame' : 'matchOnlyStrict'](lines, [
-      'expire-array@1.1.0: base64-emoji@2.0.0',
-      'expire-array@1.0.0: base64-emoji@2.0.0',
-      'expire-array@1.1.0: base64-emoji@1.0.0',
-      'expire-array@1.0.0: base64-emoji@1.0.0',
-      '//3', // after-all-results@2.0.0, c=3
-      '1/2/', // after-all-results@2.0.0, a=1 b=2
-      '//4', // after-all-results@2.0.0, c=4
-      '1/2/4', // after-all-results@2.0.0, a=1 b=2 c=4
-      '//3', // isobj@1.0.0, c=3
-      '1/2/', // isobj@1.0.0, a=1 b=2
-      'throttling-b', // throttling@1.0.1
-      'throttling-a', // throttling@1.0.2
-      'preinstall', 'pretest', 'b2f-a', 'posttest', 'pretest', 'b2f-b', 'posttest', // b2f@1.0.0
-      '1.0.0', // 27mhz@1.0.1 peerDependency
-      'patterns-a', 'patterns-b', // patterns@1.0.2
-      'patterns-a', 'patterns-b', // patterns@0.0.1
-      'roundround-a', // roundround@0.2.0
-      'roundround-a' // roundround@0.1.0
-    ])
-    t.end()
-  })
-})
-
-t.test('node version', function (t) {
-  const range = '20.x || 21.x'
-  const active = semver.satisfies(process.version, range)
-
-  t.plan(active ? 2 : 1)
-
-  process.chdir('./test/node-version')
-  const cp = start()
-
-  processStdout(cp, function (code, lines) {
-    t.equal(code, 0)
-    lines.forEach(function (line) {
-      if (!active) t.fail('this node version should not produce any output')
-      t.ok(semver.satisfies(line, range))
+    t.test('tests succeed', function (t) {
+      const cp = exec(`./index.js roundround "<=0.2.0" ${strategy} -- node -e "process.exit\\(0\\)"`)
+      cp.on('close', function (code) {
+        t.equal(code, 0)
+        t.end()
+      })
+      cp.stdout.pipe(process.stdout)
+      cp.stderr.pipe(process.stderr)
     })
-  })
-})
 
-t.test('missing "versions" property', function (t) {
-  process.chdir('./test/missing-versions')
-  let found = false
-  const cp = start({ quiet: true })
-  cp.stderr.on('data', function (chunk) {
-    if (!found && /Error: Missing "versions" property for 27mhz/.test(chunk)) {
-      found = true
-    }
-  })
-  cp.on('close', function (code) {
-    t.equal(code, 1)
-    t.ok(found)
-    t.end()
-  })
-})
-
-t.test('array of test cases', function (t) {
-  const expected = ['2.0.1', '2.0.0']
-  t.plan(3)
-
-  process.chdir('./test/array')
-  const cp = start()
-
-  processStdout(cp, function (code, lines) {
-    t.equal(code, 0)
-    lines.forEach(function (line) {
-      t.equal(line, expected.shift())
+    t.test('tests fail', function (t) {
+      const cp = exec(`./index.js roundround "<=0.2.0" ${strategy} -- node -e "process.exit\\(1\\)"`)
+      cp.on('close', function (code) {
+        t.equal(code, 1)
+        t.end()
+      })
+      cp.stdout.pipe(process.stdout)
+      cp.stderr.pipe(process.stderr)
     })
-  })
-})
 
-t.test('no matching versions', function (t) {
-  let stderr = false
+    t.test('invalid module', function (t) {
+      const cp = exec('./index.js test-all-versions-' + Date.now() + ' ^1.0.0 npm test')
+      cp.on('close', function (code) {
+        t.equal(code, 1)
+        t.end()
+      })
+      cp.stdout.pipe(process.stdout)
+      cp.stderr.pipe(process.stderr)
+    })
 
-  process.chdir('./test/no-matching-versions')
-  const cp = start({ quiet: true })
+    t.test('yaml', function (t) {
+      const cp = start({ args: strategy })
 
-  cp.stderr.on('data', function (chunk) {
-    t.equal(chunk, '-- fatal: No versions of strip-lines matching filter: 123.123.123\n')
-    stderr = true
-  })
-  processStdout(cp, function (code, lines) {
-    t.equal(code, 1, 'should exit with code 1')
-    t.equal(stderr, true, 'should output info on STDERR')
-    t.equal(lines.length, 0, 'should not run any commands')
-    t.end()
-  })
-})
+      processStdout(cp, function (code, lines) {
+        t.equal(code, 0)
+        // TODO: Remove tap16 check when Node.js 14 support is dropped
+        t[TAP16 ? 'strictSame' : 'matchOnlyStrict'](lines, [
+          'expire-array@1.1.0: base64-emoji@2.0.0',
+          'expire-array@1.0.0: base64-emoji@2.0.0',
+          'expire-array@1.1.0: base64-emoji@1.0.0',
+          'expire-array@1.0.0: base64-emoji@1.0.0',
+          '//3', // after-all-results@2.0.0, c=3
+          '1/2/', // after-all-results@2.0.0, a=1 b=2
+          '//4', // after-all-results@2.0.0, c=4
+          '1/2/4', // after-all-results@2.0.0, a=1 b=2 c=4
+          '//3', // isobj@1.0.0, c=3
+          '1/2/', // isobj@1.0.0, a=1 b=2
+          'throttling-b', // throttling@1.0.1
+          'throttling-a', // throttling@1.0.2
+          'preinstall', 'pretest', 'b2f-a', 'posttest', 'pretest', 'b2f-b', 'posttest', // b2f@1.0.0
+          '1.0.0', // 27mhz@1.0.1 peerDependency
+          'patterns-a', 'patterns-b', // patterns@1.0.2
+          'patterns-a', 'patterns-b', // patterns@0.0.1
+          'roundround-a', // roundround@0.2.0
+          'roundround-a' // roundround@0.1.0
+        ])
+        t.end()
+      })
+    })
 
-t.test('custom registry using yaml registry property', function (t) {
-  withMockRegistry({ cwd: './test/registry' }, function (path) {
-    t.equal(path, '/is-it-weekend')
-    t.end()
-  })
-})
+    t.test('node version', function (t) {
+      const range = '20.x || 21.x'
+      const active = semver.satisfies(process.version, range)
 
-t.test('custom registry using command line argument', function (t) {
-  withMockRegistry({
-    cwd: './test/registry',
-    args: '--registry=http://localhost:3001/foo/'
-  }, function (path) {
-    t.equal(path, '/foo/is-it-weekend')
-    t.end()
-  })
-})
+      t.plan(active ? 2 : 1)
 
-t.test('versions object', function (t) {
-  const expected = [
-    ...match('include: ~1.2.0', ['1.2.0', '1.2.1']),
-    ...match('include: ~1.2.0, mode: all', ['1.2.0', '1.2.1']),
-    ...match('include: ~1.0.0, exclude: 1.0.1 || 1.0.2', ['1.0.0', '1.0.3']),
-    ...match('include: <=1.4.2, mode: latest-majors', ['0.0.0', '1.4.2']),
-    ...match('include: <=1.4.2, mode: latest-minors', ['0.0.0', '1.0.3', '1.1.0', '1.2.1', '1.3.1', '1.4.2']),
-    ...match('include: <=1.4.2, mode: max-2', ['0.0.0', '1.4.2']),
-    ...match('include: <=1.4.2, mode: max-2-evenly', ['0.0.0', '1.4.2']),
-    ...match('include: <=1.4.2, mode: max-5-evenly', ['0.0.0', '1.0.1', '1.2.0', '1.4.0', '1.4.2']),
-    ...matchSemver('include: <=1.4.2, mode: max-2-random', 2, '<=1.4.2'),
-    ...match('include: <=1.4.2, mode: max-2-latest', ['1.4.1', '1.4.2']),
-    ...match('include: <=1.4.2, mode: max-2-popular', ['1.4.2', '1.4.1'])
-  ].reverse()
+      process.chdir('./test/node-version')
+      const cp = start({ args: strategy })
 
-  process.chdir('./test/versions-object')
-  const cp = start()
+      processStdout(cp, function (code, lines) {
+        t.equal(code, 0)
+        lines.forEach(function (line) {
+          if (!active) t.fail('this node version should not produce any output')
+          t.ok(semver.satisfies(line, range))
+        })
+      })
+    })
 
-  processStdout(cp, function (code, lines) {
-    t.equal(code, 0)
-    expected.forEach(function (expect, index) {
-      switch (typeof expect) {
-        case 'string':
-          t.equal(lines[index], expect)
-          break
-        case 'function':
-          expect(t, lines[index])
-          break
-        default:
-          throw new Error(`Unknown type: ${typeof expect}`)
+    t.test('missing "versions" property', function (t) {
+      process.chdir('./test/missing-versions')
+      let found = false
+      const cp = start({ args: strategy, quiet: true })
+      cp.stderr.on('data', function (chunk) {
+        if (!found && /Error: Missing "versions" property for 27mhz/.test(chunk)) {
+          found = true
+        }
+      })
+      cp.on('close', function (code) {
+        t.equal(code, 1)
+        t.ok(found)
+        t.end()
+      })
+    })
+
+    t.test('array of test cases', function (t) {
+      const expected = ['2.0.1', '2.0.0']
+      t.plan(3)
+
+      process.chdir('./test/array')
+      const cp = start({ args: strategy })
+
+      processStdout(cp, function (code, lines) {
+        t.equal(code, 0)
+        lines.forEach(function (line) {
+          t.equal(line, expected.shift())
+        })
+      })
+    })
+
+    t.test('no matching versions', function (t) {
+      let stderr = false
+
+      process.chdir('./test/no-matching-versions')
+      const cp = start({ args: strategy, quiet: true })
+
+      cp.stderr.on('data', function (chunk) {
+        t.equal(chunk, '-- fatal: No versions of strip-lines matching filter: 123.123.123\n')
+        stderr = true
+      })
+      processStdout(cp, function (code, lines) {
+        t.equal(code, 1, 'should exit with code 1')
+        t.equal(stderr, true, 'should output info on STDERR')
+        t.equal(lines.length, 0, 'should not run any commands')
+        t.end()
+      })
+    })
+
+    t.test('custom registry using yaml registry property', function (t) {
+      withMockRegistry({ cwd: './test/registry', args: strategy }, function (path) {
+        t.equal(path, '/is-it-weekend')
+        t.end()
+      })
+    })
+
+    t.test('custom registry using command line argument', function (t) {
+      withMockRegistry({
+        cwd: './test/registry',
+        args: `${strategy} --registry=http://localhost:3001/foo/`
+      }, function (path) {
+        t.equal(path, '/foo/is-it-weekend')
+        t.end()
+      })
+    })
+
+    t.test('versions object', function (t) {
+      const expected = [
+        ...match('include: ~1.2.0', ['1.2.0', '1.2.1']),
+        ...match('include: ~1.2.0, mode: all', ['1.2.0', '1.2.1']),
+        ...match('include: ~1.0.0, exclude: 1.0.1 || 1.0.2', ['1.0.0', '1.0.3']),
+        ...match('include: <=1.4.2, mode: latest-majors', ['0.0.0', '1.4.2']),
+        ...match('include: <=1.4.2, mode: latest-minors', ['0.0.0', '1.0.3', '1.1.0', '1.2.1', '1.3.1', '1.4.2']),
+        ...match('include: <=1.4.2, mode: max-2', ['0.0.0', '1.4.2']),
+        ...match('include: <=1.4.2, mode: max-2-evenly', ['0.0.0', '1.4.2']),
+        ...match('include: <=1.4.2, mode: max-5-evenly', ['0.0.0', '1.0.1', '1.2.0', '1.4.0', '1.4.2']),
+        ...matchSemver('include: <=1.4.2, mode: max-2-random', 2, '<=1.4.2'),
+        ...match('include: <=1.4.2, mode: max-2-latest', ['1.4.1', '1.4.2']),
+        ...match('include: <=1.4.2, mode: max-2-popular', ['1.4.2', '1.4.1'])
+      ].reverse()
+
+      process.chdir('./test/versions-object')
+      const cp = start({ args: strategy })
+
+      processStdout(cp, function (code, lines) {
+        t.equal(code, 0)
+        expected.forEach(function (expect, index) {
+          switch (typeof expect) {
+            case 'string':
+              t.equal(lines[index], expect)
+              break
+            case 'function':
+              expect(t, lines[index])
+              break
+            default:
+              throw new Error(`Unknown type: ${typeof expect}`)
+          }
+        })
+        t.end()
+      })
+
+      function match (prefix, versions) {
+        return versions.map((v) => `${prefix}, gunzip-maybe@${v}`)
+      }
+
+      function matchSemver (prefix, total, expectedRange) {
+        const regex = new RegExp(`^${prefix}, gunzip-maybe@(.+)$`)
+        return new Array(total).fill((t, line) => {
+          const result = line.match(regex)
+          if (!result) return t.fail(`"${line}" doesn't match regex ${regex.toString()}`)
+          t.ok(semver.satisfies(result[1], expectedRange))
+        })
       }
     })
-    t.end()
   })
-
-  function match (prefix, versions) {
-    return versions.map((v) => `${prefix}, gunzip-maybe@${v}`)
-  }
-
-  function matchSemver (prefix, total, expectedRange) {
-    const regex = new RegExp(`^${prefix}, gunzip-maybe@(.+)$`)
-    return new Array(total).fill((t, line) => {
-      const result = line.match(regex)
-      if (!result) return t.fail(`"${line}" doesn't match regex ${regex.toString()}`)
-      t.ok(semver.satisfies(result[1], expectedRange))
-    })
-  }
 })
 
 function start ({ args = '', quiet = false } = {}) {
